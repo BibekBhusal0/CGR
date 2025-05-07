59c4fb2cff57b353dfa7ee96790953e02445572b
BibekBhusal0 <bibekbhusal04@gmail.com>
2025-05-07 16:03:51 +0545

    prettier test

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,13 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { HeroUIProvider } from "@heroui/react";
+
+
+
+
+
+
+
 import { ThemeProvider } from "next-themes";
 
 const root = ReactDOM.createRoot(
