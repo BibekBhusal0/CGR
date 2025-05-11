cb23f7d4cda5bf153ae4ab862c945c08e231375e
BibekBhusal0 <bibekbhusal04@gmail.com>
2025-05-11 17:27:04 +0545

    using theme in context

--- src/components/everythingProvider.tsx
+++ src/components/everythingProvider.tsx
@@ -4,10 +4,12 @@ import { store } from "@/Logic/reducers/store";
 import { loadSettings as load } from "@/utils/storage";
 import { ReactNode, useEffect } from "react";
 import { ToastProvider } from "@heroui/toast";
+import { useTheme } from "@heroui/use-theme";
 
 type ep = { children: ReactNode };
 
 const LoadSettings = ({ children }: ep) => {
+  useTheme()
   useEffect(() => {
     load();
   }, []);
