404fe8d6390e19cdfd07227ab975cf92fb8f3be4
Bibek Bhusal <bibekbhusal04@gmail.com>
2026-01-19 18:54:13 +0545

    feat: ✨ globally disabling animation

--- src/components/everythingProvider.tsx
+++ src/components/everythingProvider.tsx
@@ -2,13 +2,15 @@ import { HeroUIProvider } from "@heroui/system";
 import { ReactNode } from "react";
 import { ToastProvider } from "@heroui/toast";
 import { useTheme } from "@heroui/use-theme";
+import { useSettingsState } from "@/Logic/state/settings";
 
 type ep = { children: ReactNode };
 
 const EverythingProvider = ({ children }: ep) => {
   useTheme();
+  const animation = useSettingsState((state) => state.animation);
   return (
-    <HeroUIProvider disableAnimation={false}>
+    <HeroUIProvider disableAnimation={!animation}>
       <ToastProvider />
       {children}
     </HeroUIProvider>
