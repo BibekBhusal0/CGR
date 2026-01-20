d187a15770b4d6bfae089900d35f22933f7e5725
Bibek Bhusal <bibekbhusal04@gmail.com>
2026-01-20 18:43:08 +0545

    refactor: ♻️ move left panel to correct file

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,51 +1,15 @@
 import { useSettingsState } from "@/Logic/state/settings";
 import FullBoard from "./full_board";
-import LeftPanel, { SettingsModal } from "./left_panel";
+import Left from "./left_panel";
 import RightPanel from "./right_panel";
-import { icons } from "@/components/icons";
-import { Button } from "@heroui/button";
 import { cn } from "@heroui/theme";
-import { Modal, ModalBody, ModalContent, ModalHeader } from "@heroui/modal";
-import { useState } from "react";
 
 function App() {
-  const [modalOpen, setModalOpen] = useState(false);
   const sidebarCollapsed = useSettingsState((state) => state.sidebarCollapsed);
-  const toggle = useSettingsState((state) => state.toggleValues);
-  const toggleSidebar = () => toggle("sidebarCollapsed");
 
   return (
     <div className="flex h-full flex-col items-start gap-3 pt-4 lg:flex-row">
-      <div
-        className={cn(
-          "pl-2 lg:mr-0",
-          sidebarCollapsed ? "flex flex-col gap-2" : "relative w-full basis-3/12 overflow-visible"
-        )}>
-        <Button
-          onPress={toggleSidebar}
-          variant={sidebarCollapsed ? "light" : "ghost"}
-          size="sm"
-          className={cn(
-            "text-xl",
-            sidebarCollapsed ? "rotate-180" : "absolute top-2 -right-15 z-50"
-          )}
-          isIconOnly>
-          {icons.others.sidebar}
-        </Button>
-        {!sidebarCollapsed && (
-          <div className="bg-content1 rounded-large shadow-medium max-h-[calc(100vh-120px)] w-full overflow-y-auto hidden lg:block">
-            <LeftPanel />
-          </div>
-        )}
-        <Button
-          variant={"light"}
-          onPress={() => setModalOpen(true)}
-          size="sm"
-          className={cn("text-xl", !sidebarCollapsed && "lg:hidden")}
-          isIconOnly>
-          {icons.left_panel.settings}
-        </Button>
-      </div>
+      <Left />
       <div
         className={cn(
           "px-2 lg:px-0",
@@ -60,14 +24,6 @@ function App() {
         )}>
         <RightPanel />
       </div>
-      <Modal isOpen={modalOpen} onOpenChange={setModalOpen} size="lg">
-        <ModalContent>
-          <ModalHeader>Settings</ModalHeader>
-          <ModalBody className="p-2">
-            <SettingsModal />
-          </ModalBody>
-        </ModalContent>
-      </Modal>
     </div>
   );
 }
--- src/app/left_panel/index.tsx
+++ src/app/left_panel/index.tsx
@@ -6,6 +6,10 @@ import Archive from "./archive";
 import { icons } from "@/components/icons";
 import { useSettingsState } from "@/Logic/state/settings";
 import { Tab, Tabs } from "@heroui/tabs";
+import { useState } from "react";
+import { Button } from "@heroui/button";
+import { Modal, ModalContent, ModalHeader, ModalBody } from "@heroui/modal";
+import { cn } from "@heroui/theme";
 
 export const Items = {
   "General Settings": { content: <GeneralSettings />, icon: icons.left_panel.settings },
@@ -51,7 +55,7 @@ function LeftPanel() {
   );
 }
 
-export function SettingsModal() {
+export function SettingsTabs() {
   const devMode = useSettingsState((state) => state.devMode);
   const modalItems = devMode ? { ...Items, ...devItems } : Items;
   return (
@@ -73,4 +77,53 @@ export function SettingsModal() {
   );
 }
 
-export default LeftPanel;
+function Left() {
+  const [modalOpen, setModalOpen] = useState(false);
+  const sidebarCollapsed = useSettingsState((state) => state.sidebarCollapsed);
+  const toggle = useSettingsState((state) => state.toggleValues);
+  const toggleSidebar = () => toggle("sidebarCollapsed");
+
+  return (
+    <>
+      <div
+        className={cn(
+          "pl-2 lg:mr-0",
+          sidebarCollapsed ? "flex flex-col gap-2" : "relative w-full basis-3/12 overflow-visible"
+        )}>
+        <Button
+          onPress={toggleSidebar}
+          variant={sidebarCollapsed ? "light" : "ghost"}
+          size="sm"
+          className={cn(
+            "text-xl",
+            sidebarCollapsed ? "rotate-180" : "absolute top-2 -right-15 z-50"
+          )}
+          isIconOnly>
+          {icons.others.sidebar}
+        </Button>
+        {!sidebarCollapsed && (
+          <div className="bg-content1 rounded-large shadow-medium max-h-[calc(100vh-120px)] w-full overflow-y-auto hidden lg:block">
+            <LeftPanel />
+          </div>
+        )}
+        <Button
+          variant={"light"}
+          onPress={() => setModalOpen(true)}
+          size="sm"
+          className={cn("text-xl", !sidebarCollapsed && "lg:hidden")}
+          isIconOnly>
+          {icons.left_panel.settings}
+        </Button>
+      </div>
+      <Modal isOpen={modalOpen} onOpenChange={setModalOpen} size="lg">
+        <ModalContent>
+          <ModalHeader>Settings</ModalHeader>
+          <ModalBody className="p-2">
+            <SettingsTabs />
+          </ModalBody>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+}
+export default Left;
