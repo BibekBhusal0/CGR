48c50ae7b0533bd9b003b06d1b3629420504b82d
Bibek Bhusal <bibekbhusal04@gmail.com>
2026-01-20 16:07:51 +0545

    fix: 🐛 width for main layout

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -5,13 +5,13 @@ import RightPanel from "./right_panel";
 function App() {
   return (
     <div className="flex h-full flex-col items-start gap-3 pt-6 md:flex-row">
-      <div className="basis-3/12">
+      <div className="basis-3/12 w-full">
         <LeftPanel />
       </div>
-      <div className="basis-6/12 lg:basis-5/12">
+      <div className="basis-6/12 lg:basis-5/12 px-2 lg:px-0 ">
         <FullBoard />
       </div>
-      <div className="relative basis-3/12 px-2 lg:basis-4/12">
+      <div className="relative basis-3/12 px-2 lg:basis-4/12 w-full">
         <RightPanel />
       </div>
     </div>
