0684e0f66f2b983c998f092a42793b90e27749a1
Bibek Bhusal <bibekbhusal04@gmail.com>
2026-02-04 17:57:30 +0545

    fix: 🐛 correct calculation for eval

--- src/Logic/analyze.ts
+++ src/Logic/analyze.ts
@@ -37,7 +37,7 @@ export interface analysisType extends StockfishOutput {
 }
 
 function capEval(e: evaluationType, cap: number = 50): number {
-  if (e.type === "mate") return e.value > 0 ? cap : -cap;
+  if (e.type === "mate") return e.value > 0 ? cap / 100 : -cap / 100;
   if (e.value > cap) return cap;
   if (e.value < -cap) return -cap;
   return e.value;
