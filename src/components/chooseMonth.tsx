3ce864df6828db31113011e10fe963d66e4c2abb
Bibek Bhusal <bibekbhusal04@gmail.com>
2026-06-27 19:02:16 +0545

    fix: 🐛 choose month correct not allowing to choose other days

--- src/components/chooseMonth.tsx
+++ src/components/chooseMonth.tsx
@@ -23,13 +23,6 @@ export default function ChooseMonth({ onClick }: chooseMonthProps) {
     };
   };
 
-  const changeMonth = (n: number) => {
-    const add = n > 0;
-    n = Math.abs(n);
-    const newDate = add ? start.add({ months: n }) : start.subtract({ months: n });
-    setStart(newDate);
-  };
-
   return (
     <DateRangePicker
       endName="endDate"
@@ -43,28 +36,31 @@ export default function ChooseMonth({ onClick }: chooseMonthProps) {
         <RangeCalendar
           aria-label="Choose trip dates"
           value={getRange()}
+          onFocusChange={(date) => {
+            setStart(startOfMonth(date));
+          }}
           maxValue={today(getLocalTimeZone())}>
           <RangeCalendar.Header>
-            {/* <RangeCalendar.YearPickerTrigger> */}
-            {/*   <RangeCalendar.YearPickerTriggerHeading /> */}
-            {/*   <RangeCalendar.YearPickerTriggerIndicator /> */}
-            {/* </RangeCalendar.YearPickerTrigger> */}
-            <RangeCalendar.NavButton slot="previous" onClick={() => changeMonth(-1)} />
-            <RangeCalendar.NavButton slot="next" onClick={() => changeMonth(1)} />
+            <RangeCalendar.YearPickerTrigger>
+              <RangeCalendar.YearPickerTriggerHeading />
+              <RangeCalendar.YearPickerTriggerIndicator />
+            </RangeCalendar.YearPickerTrigger>
+            <RangeCalendar.NavButton slot="previous" />
+            <RangeCalendar.NavButton slot="next" />
           </RangeCalendar.Header>
           <RangeCalendar.Grid>
             <RangeCalendar.GridHeader>
               {(day) => <RangeCalendar.HeaderCell>{day}</RangeCalendar.HeaderCell>}
             </RangeCalendar.GridHeader>
             <RangeCalendar.GridBody>
-              {(date) => <RangeCalendar.Cell date={date} />}
+              {(date) => <RangeCalendar.Cell className="pointer-events-none" date={date} />}
             </RangeCalendar.GridBody>
           </RangeCalendar.Grid>
-          {/* <RangeCalendar.YearPickerGrid> */}
-          {/*   <RangeCalendar.YearPickerGridBody> */}
-          {/*     {({ year }) => <RangeCalendar.YearPickerCell year={year} />} */}
-          {/*   </RangeCalendar.YearPickerGridBody> */}
-          {/* </RangeCalendar.YearPickerGrid> */}
+          <RangeCalendar.YearPickerGrid>
+            <RangeCalendar.YearPickerGridBody>
+              {({ year }) => <RangeCalendar.YearPickerCell year={year} />}
+            </RangeCalendar.YearPickerGridBody>
+          </RangeCalendar.YearPickerGrid>
         </RangeCalendar>
         <div className="flex-center w-full">
           <Button
